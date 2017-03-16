23084fc6d951f35050c7ebeb9bfa657d01a87997
RicardoClaros <ricardoclaros25@gmail.com>
2017-03-16 00:21:31 -0700

    File path fix

--- js/Main.js
+++ js/Main.js
@@ -1,7 +1,7 @@
 // Excersise for beginner todolist
 function todoList() {
   var item = document.getElementById("todoInput").value
-  var text = document.createtextNode(item)
+  var text = document.createTextNode(item)
   var newItem = document.createElement("Li")
   newItem.appendChild(text)
   document.getElementById("todoList").appendChild(newItem)
